57c21e1e06e82e539b00a4179b87de1760604285
huahangwanghao <1178952125@qq.com>
2017-11-23 15:36:24 +0800

    先提交上去

---
+++ src/main/java/com/wanghao/thread/NewThread.java
@@ -0,0 +1,18 @@
+package com.wanghao.thread;
+
+public class NewThread implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("新线程:"+Thread.currentThread().getName());
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Thread(new NewThread()).start();
+		System.out.println("主方法:"+Thread.currentThread().getName());
+
+	}
+
+}
